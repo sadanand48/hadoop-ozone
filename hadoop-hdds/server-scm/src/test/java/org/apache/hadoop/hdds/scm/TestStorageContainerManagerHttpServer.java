8639762d352897be56faaf0e29d5f46d630a7b7e
Sadanand Shenoy <sadanand.shenoy@cloudera.com>
2023-03-16 20:02:31 +0530

    Revert "HDDS-6176. Ozone service WebUI is not accessible with 404 error. (#4308)"
    This reverts commit 3cc079478b47ea976b5872a7e80b69f5c6ed1501.

--- hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/TestStorageContainerManagerHttpServer.java
+++ hadoop-hdds/server-scm/src/test/java/org/apache/hadoop/hdds/scm/TestStorageContainerManagerHttpServer.java
@@ -56,8 +56,7 @@ public class TestStorageContainerManagerHttpServer {
   public static void setUp() throws Exception {
     File base = new File(BASEDIR);
     FileUtil.fullyDelete(base);
-    File ozoneMetadataDirectory = new File(BASEDIR, "metadata");
-    ozoneMetadataDirectory.mkdirs();
+    base.mkdirs();
     conf = new OzoneConfiguration();
     keystoresDir = new File(BASEDIR).getAbsolutePath();
     sslConfDir = KeyStoreTestUtil.getClasspathDir(
@@ -69,8 +68,6 @@ public class TestStorageContainerManagerHttpServer {
         KeyStoreTestUtil.getClientSSLConfigFileName());
     conf.set(OzoneConfigKeys.OZONE_SERVER_HTTPS_KEYSTORE_RESOURCE_KEY,
         KeyStoreTestUtil.getServerSSLConfigFileName());
-    conf.set(OzoneConfigKeys.OZONE_METADATA_DIRS,
-        ozoneMetadataDirectory.getAbsolutePath());
   }
 
   @AfterAll
